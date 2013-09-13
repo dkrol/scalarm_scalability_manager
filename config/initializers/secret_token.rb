329ddb313be862461ac86a5d84d10238838f2d8d
Dariusz Krol <dkrol@agh.edu.pl>
2013-09-13 14:58:12 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScalarmScalabilityManager::Application.config.secret_key_base = '8344b013560e840388160655127778af824bbbbf3580035379948e0e605913ed07da2f7d12b430089e8e23505455f0f5f8d4628e39b7e2b477cba539d12ec922'
